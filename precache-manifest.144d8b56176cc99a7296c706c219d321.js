091d3c2869ed152d6c1ae831bcec349e01b953c8
Yuliia Batrakova <yuliya.batrakova@gmail.com>
2019-06-05 23:15:14 +0300

    Updates

---
+++ precache-manifest.144d8b56176cc99a7296c706c219d321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "52de8b29dcded5684f93",
+    "url": "/robofriends/static/js/main.72fa67ad.chunk.js"
+  },
+  {
+    "revision": "4e74336e48417fadb7fc",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "52de8b29dcded5684f93",
+    "url": "/robofriends/static/css/main.83eecb1d.chunk.css"
+  },
+  {
+    "revision": "4e74336e48417fadb7fc",
+    "url": "/robofriends/static/css/2.09833e07.chunk.css"
+  },
+  {
+    "revision": "c6718582f382c5acca1ce4fe26e78640",
+    "url": "/robofriends/index.html"
+  }
+];
